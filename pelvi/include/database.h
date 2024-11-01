2c11684a01b3c4252c90975106bb511cf6931e67
Volker Wengert <22374206+vwengert@users.noreply.github.com>
2024-11-01 11:34:43 +0100

    adding the initializing from new database

--- pelvi/include/database.h
+++ pelvi/include/database.h
@@ -1,29 +1,27 @@
-#ifndef DATABASE_H
-#define DATABASE_H
+#pragma once
 
-//#include <QSqlDatabase>
+#include <QSqlDatabase>
 #include <QVariantList>
-
-
 
 class Database : public QObject
 {
-  Q_OBJECT
-public:
-  explicit Database(QObject *parent = nullptr);
-  ~Database();
+    Q_OBJECT
 
-  Database(const Database &) = delete;
-  Database(Database &&) = delete;
-  Database &operator=(const Database &) = delete;
-  Database &operator=(Database &&) = delete;
+  public:
+    explicit Database( QObject* parent = nullptr );
+    ~Database();
 
-  bool openDatabase(const QString &path);
-  void closeDatabase();
-  bool executeQuery(const QString &queryStr, const QVariantList &args = QVariantList());
+    Database( const Database& ) = delete;
+    Database( Database&& ) = delete;
+    Database& operator=( const Database& ) = delete;
+    Database& operator=( Database&& ) = delete;
 
-private:
-  //  QSqlDatabase m_db;
+    bool openDatabase( const QString& path );
+    void closeDatabase();
+    bool executeQuery( const QString& queryStr, const QVariantList& args = QVariantList() );
+    void initializeDatabase();
+    bool isDatabaseInitialized( const QString& expectedVersion );
+
+  private:
+    QSqlDatabase m_db;
 };
-
-#endif // DATABASE_H
--- pelvi/src/database.cpp
+++ pelvi/src/database.cpp
@@ -1,50 +1,81 @@
 #include "database.h"
-#include <QSqlQuery>
-#include <QSqlError>
+
+#include "dbinit.h"
+
 #include <QDebug>
+#include <QSqlDatabase>
+#include <QSqlError>
+#include <QSqlQuery>
 
-Database::Database(QObject *parent) : QObject(parent)
+Database::Database( QObject* parent )
+  : QObject( parent )
 {
 }
 
-Database::~Database()
-{
-  closeDatabase();
-}
+Database::~Database() { closeDatabase(); }
 
-bool Database::openDatabase(const QString &path)
+bool Database::openDatabase( const QString& path )
 {
-  // m_db = QSqlDatabase::addDatabase("QSQLITE");
-  // m_db.setDatabaseName(path);
+  m_db = QSqlDatabase::addDatabase( "QSQLITE" );
+  m_db.setDatabaseName( path );
 
-  // if (!m_db.open()) {
-  //   qDebug() << "Error: connection with database failed";
-  //   return false;
-  // }
-  // qDebug() << "Database: connection ok";
+  if(!m_db.open())
+  {
+    qDebug() << "Error: connection with database failed";
+    return false;
+  }
+  if(!isDatabaseInitialized( "1" ))
+  {
+    qDebug() << "Initializing database";
+    initializeDatabase();
+  }
+  qDebug() << "Database: connection ok";
   return true;
 }
 
 void Database::closeDatabase()
 {
-  // if (m_db.isOpen()) {
-  //   m_db.close();
-  // }
+  if(m_db.isOpen())
+  {
+    m_db.close();
+  }
 }
 
-bool Database::executeQuery(const QString &queryStr, const QVariantList &args)
+bool Database::executeQuery( const QString& queryStr, const QVariantList& args )
 {
-  // QSqlQuery query(m_db);
-  // query.prepare(queryStr);
+  QSqlQuery query( m_db );
+  query.prepare( queryStr );
 
-  // for (int i = 0; i < args.size(); ++i) {
-  //   query.bindValue(i, args[i]);
-  // }
+  for(int i = 0; i < args.size(); ++i)
+  {
+    query.bindValue( i, args[ i ] );
+  }
 
-  // if (!query.exec()) {
-  //   qDebug() << "Error: query execution failed -" << query.lastError();
-  //   return false;
-  // }
+  if(!query.exec())
+  {
+    qDebug() << "Error: query execution failed -" << query.lastError();
+    return false;
+  }
 
   return true;
+}
+
+bool Database::isDatabaseInitialized( const QString& expectedVersion )
+{
+  QSqlQuery query( m_db );
+  query.prepare( "SELECT version FROM database" );
+
+  if(!query.exec() || !query.next())
+  {
+    return false;
+  }
+
+  QString version = query.value( 0 ).toString();
+  return version == expectedVersion;
+}
+
+void Database::initializeDatabase()
+{
+  DbInit initializer;
+  initializer.initializeDatabase( m_db );
 }
